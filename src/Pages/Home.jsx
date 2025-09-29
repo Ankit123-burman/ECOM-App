59177e086dfda85fd3a8d3ea6f6dd0d458b87e29
priyanshuburman540@gmail.com <151107409+Ankit123-burman@users.noreply.github.com>
2025-09-29 11:55:29 +0530

    Fixed ProductDetails component (thumbnails, mobile scroll, typo fixes)

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../component/layout/Hero'
 import GenderCollection from '../Products/GenderCollection'
 import New from '../Products/New'
+import { ProductDetails } from '../Products/ProductDetails'
 
 function Home() {
   return (
@@ -9,6 +10,9 @@ function Home() {
         <Hero/>
         <GenderCollection/>
         <New/>
+
+        <h2 className='text-3xl text-center font-bold mb-4' >Best Seller</h2>
+        <ProductDetails/>
     </div>
   )
 }
