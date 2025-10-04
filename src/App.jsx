b9c138f301f628e0bd7e4ab7c39b710d85bff25a
priyanshuburman540@gmail.com <151107409+Ankit123-burman@users.noreply.github.com>
2025-10-04 20:09:20 +0530

    add login register and profile page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import UserLayout from './component/layout/UserLayout'
 import Home from './Pages/Home'
 import {Toaster} from "sonner"
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import Profile from './Pages/Profile'
 
 function App() {
   return (
@@ -12,7 +15,11 @@ function App() {
         <Routes>
           <Route path='/' element={<UserLayout />} >
              <Route index element={<Home/>}/>
+             <Route path="/login" element={<Login/>}/>
+             <Route path="/register" element={<Register/>}/>
+             <Route path="/profile" element={<Profile/>}/>
           </Route>
+          {/*admin route*/}
         </Routes>
       </BrowserRouter>
     </>
